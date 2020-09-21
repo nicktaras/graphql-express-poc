e2450c85185dbc01202124ab978d32e0114c9898
Nick Taras <nicktaras@hotmail.co.uk>
2020-09-21 16:30:28 +1000

    poc

--- index.js
+++ index.js
@@ -2,41 +2,48 @@ var express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 var { buildSchema } = require('graphql');
 
+// This is for Proof of Concept only.
+
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
   type Query {
     createUser(name: String, pass: String): String
+    updateUser(name: String, pass: String): String
     readUser: String
-    updateUser: String
     deleteUser: String
   }
 `);
 
-let user = "";
+// Temp Storage to memory
+let application = {};
 
 // The root provides a resolver function for each API endpoint
 var root = {
   createUser: ({ name, pass }) => {
     // CreateUser(name, pass);
-    user = name;
-    return `User Profile for ${user} has been Created`;
+    if (application['user']) return 'User exists';
+    application['user'] = name;
+    return `User Profile for ${application.user} has been Created`;
   },
   readUser: () => {
-    return user;
+    return application.user;
   },
-  updateUser: () => {
-    return user;
+  updateUser: ({ name, pass }) => {
+    application['user'] = name;
+    return `User Profile for ${application.user} has been Updated`;
   },
   deleteUser: () => {
-    return `User Profile for ${user} has been Deleted`;
+    return `User Profile for ${application.user} has been Deleted`;
   }
 };
 
 var app = express();
+
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   rootValue: root,
   graphiql: true,
 }));
+
 app.listen(4000);
 console.log('Running a GraphQL API server at localhost:4000/graphql');
